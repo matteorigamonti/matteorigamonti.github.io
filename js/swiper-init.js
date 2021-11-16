ef4f9ec7af905c4376810b75972ddadd18e15b69
matteo_rigamonti@hotmail.it <MatteoRigamonti>
2021-11-16 20:50:00 +0100

    Fix site, add .gitignore

--- js/swiper-init.js
+++ js/swiper-init.js
@@ -1,15 +1,13 @@
 // SWIPER
 var swiper = new Swiper(".card-slider-container", {
 	spaceBetween: 25,
+	slidesPerView: 1,
 	breakpoints: {
-		1024: {
-			slidesPerView: 3,
-		},
 		768: {
 			slidesPerView: 2,
 		},
-		425: {
-			slidesPerView: 1,
+		1024: {
+			slidesPerView: 3,
 		},
 	},
 	centeredSlides: false,
